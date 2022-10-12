c4b194e6ebcdf806f0b64214629262bf5a60496c
Damian Zalewski <damian_zalewskipl@o2.pl>
2022-10-12 07:05:06 +0200

    added face recognition

--- 04 - Image Face Recognition/script.js
+++ 04 - Image Face Recognition/script.js
@@ -1,4 +1,4 @@
-const imageUpload = document.querySelector('imageUpload');
+const imageUpload = document.querySelector('#imageUpload');
 
 Promise.all([
     faceapi.nets.faceRecognitionNet.loadFromUri('models'),
@@ -7,5 +7,23 @@ Promise.all([
 ]).then(start);
 
 function start(){
+    const container = document.createElement('div');
+    container.style.position = 'relative';
+    document.body.append(container);
     document.body.append('Loaded');
+    imageUpload.addEventListener('change', async () => {
+        const image = await faceapi.bufferToImage(imageUpload.files[0]);
+        container.append(image);
+        const canvas = faceapi.createCanvasFromMedia(image);
+        container.append(canvas);
+        const displaySize = {width: image.width, height: image.height};
+        faceapi.matchDimensions(canvas, displaySize);
+        const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors();
+        const resizedDetections = faceapi.resizeResults(detections, displaySize);
+        resizedDetections.forEach(detection => {
+            const box = detection.detection.box;
+            const drawBox = new faceapi.draw.DrawBox(box, {label: 'Face'});
+            drawBox.draw(canvas);
+        });
+    });
 }
