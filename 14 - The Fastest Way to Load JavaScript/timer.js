db238e6d74914ae33d31d5ec04bdd99a9e0d8e44
Damian Zalewski <damian_zalewskipl@o2.pl>
2022-10-31 06:44:16 +0100

    Finished The fastest way to load javascript

---
+++ 14 - The Fastest Way to Load JavaScript/timer.js
@@ -0,0 +1,5 @@
+console.log("Start HTML Parse: " + performance.now() + " in milliseconds.");
+
+window.onload = function() {
+    console.log("HTML Loaded: " + performance.now() + " in milliseconds.");
+}
