8600e0cfcb1cc68d61d66eea1365ebba27439afc
Damian Zalewski <damian_zalewskipl@o2.pl>
2022-11-07 06:19:27 +0100

    finished differences between variables lesson

---
+++ 18 - Differences Between Var Let Const/script.js
@@ -0,0 +1,34 @@
+if(true){
+    var varVariable = 'var';
+}
+
+console.log(varVariable);
+
+var varVariable = 'Another var!';
+
+console.log(varVariable)
+
+console.log(lateVar);
+var lateVar = 'true';
+
+const player = {
+    name: 'Bob'
+}
+
+console.log(player);
+
+player.name = 'John';
+
+console.log(player);
+
+
+if(true){
+    let letVariable = 'let';
+}
+
+console.log(letVariable);
+
+const constVariable = 1;
+constVariable = 2;
+
+console.log(constVariable);
